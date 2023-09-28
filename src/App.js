6a9bcf835865b60703333f1a4dd939359992c2cc
arkoholic <ebenezerocraharkoh2001@gmail.com>
2023-09-28 22:11:49 +0000

    A new project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          We will be building a website very soon!
+          This will be exciting
         </p>
         <a
           className="App-link"
