402a56785b987ab32d561456501b097cce8c795a
arkoholic <ebenezerocraharkoh2001@gmail.com>
2023-09-29 13:58:46 +0000

    learnt destructing props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import './App.css';
 // import Hello from './components/Hello';
 // import Message from './components/Message';
-import Counter from './components/Counter';
+//import Counter from './components/Counter';
 //import Profile from './components/Profile';
 //import Message from './components/Message';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div className="App">
-     <Counter/>
+     <Resume name="Mr. Incredible"/>
     </div>
   );
 }
