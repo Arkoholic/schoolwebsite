a3758f38cbc9e0eb9542ec042465814803d08ba7
arkoholic <ebenezerocraharkoh2001@gmail.com>
2023-10-05 00:13:35 +0000

    massive changes with @Lawrence😂

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import {Component} from 'react';
-import '../styles.css';
-import logo from '../img/School_logo.svg';
+import '../Header.css';
+import logo from '../assets/img/School_logo.svg';
 
 class Header extends Component {
  render(){
     return (
         <>
         <div className='Header'>
-            <div><img src={logo} alt='School logo' /> </div>
-            <div style={{display: 'flex', flexDirection:'column', textAlign:'center'}}> 
-            <h1 className='large-heading' style={{textAlign:'justify'}}>ABUNDANT RAIN SCHOOL</h1>  
-            <h4 className='sub-heading'><i>Let God arise!</i></h4>
-        </div>
-        </div>
+    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <img src={logo} alt='School logo' style={{ width: 46, height: 53 }} />
+        <h1 className='logo-large-heading' style={{ textAlign: 'justify' }}>ABUNDANT RAIN SCHOOL</h1>
+    </div>
+    <ul className='nav-bar-options'>
+        <li><a href='' className='link'>Home</a></li>
+        <li><a href='' className='link'>About Us</a></li>
+        <li><a href='' className='link'>Admissions</a></li>
+        <li><a href='' className='link'>Gallery</a></li>
+    </ul>
+</div>
+
+        
+        
         
         </>
         
