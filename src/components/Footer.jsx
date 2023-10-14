f0774cb9bf2baf9731180e17a204cbe013c91b2a
arkoholic <ebenezerocraharkoh2001@gmail.com>
2023-10-14 21:49:47 +0000

    worked on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,43 @@ export default function Footer() {
         <img src={logo} alt='logo'style={{ width: 30, height: 34.5 }} />
         <span className='schooltitle'>ABUNDANT RAIN SCHOOL</span>
         </div>
+        <div></div>
+        <div className='schoolHours'>
+        <div className='hours'>
+            <span className='subHeadings'>School Hours</span>
+            <div className='DOTWhead'>
+            <span className='DOTW'>Monday</span>
+            <span className='time'>7:00-5:00pm</span>
+            </div>
+            <div className='DOTWhead'>
+            <span className='DOTW'>Tuesday</span>
+            <span className='time'>7:00-5:00pm</span>
+            </div>
+            <div className='DOTWhead'>
+            <span className='DOTW'>Wednesday</span>
+            <span className='time'>7:00-5:00pm</span>
+            </div>
+            <div className='DOTWhead'>
+            <span className='DOTW'>Thursday</span>
+            <span className='time'>7:00-5:00pm</span>
+            </div>
+            <div className='DOTWhead'>
+            <span className='DOTW'>Friday</span>
+            <span className='time'>7:00-5:00pm</span>
+            </div>
+        </div>
+        <div className='address'>
+        <span className='subHeadings'>Address</span> <br/>
+        <span className='content'>Street Name</span> <br/>
+        <span className='content'>GM-025 2455</span>
+        </div>
+        <div className='contact'>
+        <span className='subHeadings'>Contact</span> <br/>
+        <span className='content'>+233 255845252</span> <br/>
+        <span className='content'>abundantrainschool@gmail.com</span>
+        </div>
+        </div>
+        <div className='solo'>Let God arise !!</div>
         </div>
     );
 }
